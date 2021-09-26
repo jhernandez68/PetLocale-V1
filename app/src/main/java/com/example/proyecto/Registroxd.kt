5b0b5f3b37f7e9ca465eef7993822e5c00493085
JHernandez-68 <56227740+JHernandez-68@users.noreply.github.com>
2021-09-26 18:57:30 -0500

    Logeoadded

--- app/src/main/java/com/example/proyecto/Registroxd.kt
+++ app/src/main/java/com/example/proyecto/Registroxd.kt
@@ -4,22 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_logeo.*
-import kotlinx.android.synthetic.main.activity_tutorial.*
+import kotlinx.android.synthetic.main.activity_registroxd.*
 
-class Logeo : AppCompatActivity() {
+class Registroxd : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_logeo)
+        setContentView(R.layout.activity_registroxd)
 
         val intent = Intent(this, Tutorial::class.java)
-        entrar.setOnClickListener(){
+        registrarse.setOnClickListener(){
             startActivity(intent)
-        }
-
-        val intent2 = Intent(this, Registroxd::class.java)
-
-        textView3.setOnClickListener(){
-            startActivity(intent2)
         }
     }
 }
