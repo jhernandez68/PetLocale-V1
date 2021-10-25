3b2d71a94e5bdd22c50346371fb9b46cf83ca254
JHernandez-68 <56227740+JHernandez-68@users.noreply.github.com>
2021-10-24 19:17:42 -0500

    V2 with database

--- app/src/main/java/com/example/proyecto/Logeo.kt
+++ app/src/main/java/com/example/proyecto/Logeo.kt
@@ -3,6 +3,8 @@ package com.example.proyecto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_logeo.*
 import kotlinx.android.synthetic.main.activity_tutorial.*
 
@@ -10,16 +12,33 @@ class Logeo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logeo)
+        entrar.setOnClickListener{
+            if(password.text.isNotEmpty() && password.text.isNotEmpty() && username.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(username.text.toString()
+                    , password.text.toString()).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        showMain(it.result?.user?.email?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
 
-        val intent = Intent(this, Tutorial::class.java)
-        entrar.setOnClickListener(){
-            startActivity(intent)
         }
+    }
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error registrando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
 
-        val intent2 = Intent(this, Registroxd::class.java)
+    private fun showMain(email: String, provider: ProviderType){
+        val homeIntent = Intent(this,MainActivity::class.java).apply {
 
-        textView3.setOnClickListener(){
-            startActivity(intent2)
         }
+        startActivity(homeIntent)
     }
 }
